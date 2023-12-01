652d93325eae01fb503246f7502128606807002a
Alexander Mirvis <45396441+LynxGeekNYC@users.noreply.github.com>
2023-11-30 22:48:32 -0500

    Create single-all.sh

---
+++ single-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+COUNTER=$1
+for (( c=1; c<=COUNTER; c++))
+do
+./all
+ cp callfile.bak callfile.call
+ chmod 777 callfile.call
+ chown asterisk:asterisk callfile.call
+ mv callfile.call /var/spool/asterisk/outgoing/
+sleep 1
+echo "" 
+echo -e "\e[31mPushing Victim to Bridge:\e[0m"
+ sed -n 1p callfile.bak 
+ echo ""
+done
